a46ea6cf6b5c3af9f2d7ed4c49534b11dd02d59f
senti <brianmv@uci.edu>
2023-04-13 13:56:16 -0700

    ok finally looks better

--- packages/core/src/lib/auth.js/index.ts
+++ packages/core/src/lib/auth.js/index.ts
@@ -1,7 +1,11 @@
 import { merge } from "$lib/utils/merge"
 import type { Override } from "$lib/utils/override"
 import type { Awaitable } from "$lib/utils/promise"
-import type { Client, OAuth2TokenEndpointResponse, OpenIDTokenEndpointResponse } from "oauth4webapi"
+import type { 
+  Client,
+  OAuth2TokenEndpointResponse,
+  OpenIDTokenEndpointResponse
+} from "oauth4webapi"
 
 interface OAuthProviderButtonStyles {
   logo: string
@@ -83,7 +87,7 @@ type TokenEndpointHandler = EndpointHandler<
 
 type UserinfoEndpointHandler = EndpointHandler<UrlParams, { tokens: TokenSet }, Profile>
 
-interface InternalEndpointMetadata {
+interface OAuthEndpoints {
   authorization?: { 
     url: URL
   }
@@ -98,40 +102,131 @@ interface InternalEndpointMetadata {
   }
 }
 
-type ProviderType = "oidc" | "oauth" | "email" | "credentials"
-
-interface CommonProviderOptions {
+/**
+ * Shared options for all providers.
+ */
+interface ProviderOptions {
+  /**
+   * Unique identifier for the provider. Can appear in the URL.
+   */
   id: string
+
+  /**
+   * Provider name. Can be displayed on auth buttons.
+   */
   name: string
+
+  /**
+   * @see {@link ProviderType}
+   */
   type: ProviderType
 }
 
-interface OAuth2Config<Profile> extends CommonProviderOptions, PartialIssuer {
+/**
+ * Base OAuth config.
+ */
+interface OAuth2Config<Profile> extends ProviderOptions, PartialIssuer {
+  /**
+   * Identifies the provider. Can be in URL, e.g. /auth/callback/:providerId
+   */
   id: string
+
+  /**
+   * The name of the provider. Can be shown on auth buttons.
+   */
   name: string
-  wellKnown?: string
+
+  /**
+   * OpenID Connect (OIDC) compliant providers can configure this instead of {@link OAuthEndpoints}.
+   * Can still manually configure {@link OAuthEndpoints} for advanced controls.
+   *
+   * [Authorization Server Metadata](https://datatracker.ietf.org/doc/html/rfc8414#section-3)
+   */
   issuer?: string
+
+  /**
+   * Manually configure OIDC provider discovery endpoint.
+   */
+  wellKnown?: string
+
+  /**
+   * 1. Initiate OAuth process by sending the user to this URL.
+   *
+   * [Authorization endpoint](https://datatracker.ietf.org/doc/html/rfc6749#section-3.1)
+   */
   authorization?: string | AuthorizationEndpointHandler
+
+  /**
+   * 2. After user authenticates and redirects, exchange the provided code for tokens.
+   *
+   * [Token endpoint](https://datatracker.ietf.org/doc/html/rfc6749#section-3.2)
+   */
   token?: string | TokenEndpointHandler
+
+  /**
+   * 3. After getting tokens, use the access token to authorize request for user info.
+   *
+   * [Userinfo endpoint](https://openid.net/specs/openid-connect-core-1_0.html#UserInfo)
+   */
   userinfo?: string | UserinfoEndpointHandler
+
+  /**
+   * Provider type.
+   */
   type: "oauth"
+
+  /**
+   * Receives the profile object returned by the OAuth provider, and returns a user object.
+   */
   profile?: (profile: Profile, tokens: TokenSet) => Awaitable<User>
+
+   /**
+   * The CSRF protection performed on the callback endpoint.
+   * @default ["pkce"]
+   *
+   * [RFC 7636 - Proof Key for Code Exchange by OAuth Public Clients (PKCE)](https://www.rfc-editor.org/rfc/rfc7636.html#section-4) |
+   * [RFC 6749 - The OAuth 2.0 Authorization Framework](https://www.rfc-editor.org/rfc/rfc6749.html#section-4.1.1) |
+   * [OpenID Connect Core 1.0](https://openid.net/specs/openid-connect-core-1_0.html#IDToken) |
+   */
   checks?: ('pkce' | 'state' | 'none' | 'nonce')[]
+
+  /**
+   * The OAuth client ID.
+   */
   clientId?: string
+
+  /**
+   * The OAuth client secret.
+   */
   clientSecret?: string
+
+  /**
+   * A client object that can be used by `oauth4webapi` to make requests to the provider.
+   */
   client?: Partial<Client>
+
+  /**
+   * @ignore Used by Auth.js for buttons.
+   */
   style?: OAuthProviderButtonStyles
-  allowDangerousEmailAccountLinking?: boolean
+
+  /**
+   * User-defined options. Will be deep-merged with the provider's default options.
+   */
   options?: OAuthUserConfig<Profile>
 }
 
-type InternalConfigOptions = { options?: Record<string, unknown> }
-
+/**
+ * Any provider config.
+ */
 type AnyConfig<P = any> = OIDCConfig<P> | OAuth2Config<P>
 
+/**
+ * Internally generated OAuth config.
+ */
 type OAuthConfigInternal<Profile> = Override<
   OAuthConfig<Profile>, 
-  Required<Pick<OAuthConfig<Profile>, "clientId" | "checks" | "profile">> & InternalEndpointMetadata
+  Required<Pick<OAuthConfig<Profile>, "clientId" | "checks" | "profile">> & OAuthEndpoints
 > 
 
 /** 
@@ -144,7 +239,17 @@ type InternalProvider<T extends ProviderType = ProviderType> = InternalUrls & (
   T extends "credentials" ? never /* ? CredentialsConfig */ : never
 )
 
-export type Provider<P extends Profile = Profile> = AnyConfig<P> & InternalConfigOptions
+/*
+ * Providers passed to Auth.js must define one of these types.
+ *
+ * @see [RFC 6749 - The OAuth 2.0 Authorization Framework](https://www.rfc-editor.org/rfc/rfc6749.html#section-2.3)
+ * @see [OpenID Connect Core 1.0](https://openid.net/specs/openid-connect-core-1_0.html#ClientAuthentication)
+ * @see [Email or Passwordless Authentication](https://authjs.dev/concepts/oauth)
+ * @see [Credentials-based Authentication](https://authjs.dev/concepts/credentials)
+ */
+export type ProviderType = "oidc" | "oauth" | "email" | "credentials"
+
+export type Provider<P extends Profile = Profile> = AnyConfig<P> & OAuthUserConfig<P>
 
 export type OIDCConfig<Profile> = Override<OAuth2Config<Profile>, { type: "oidc" }>
 
@@ -159,6 +264,9 @@ export type OIDCUserConfig<Profile> =
   Omit<Partial<OIDCConfig<Profile>>, "options" | "type"> &
   Required<Pick<OIDCConfig<Profile>, "clientId" | "clientSecret">>
 
+/**
+ * Process an array of providers.
+ */
 export default function parseProviders(
   params: { providers: Provider[], url: URL }
 ): InternalProvider[] {
@@ -185,7 +293,10 @@ export default function parseProviders(
   return providers
 }
 
-function normalizeOAuth(c: AnyConfig & InternalConfigOptions): OAuthConfigInternal<any> {
+/**
+ * Normalize OAuth providers to the common interface.
+ */
+function normalizeOAuth(c: AnyConfig & OAuthUserConfig<any>): OAuthConfigInternal<any> {
   const authorization = normalizeEndpoint<'authorization'>(c.authorization, c.issuer)
 
   if (authorization && !authorization.url?.searchParams.has("scope")) {
@@ -212,6 +323,9 @@ function normalizeOAuth(c: AnyConfig & InternalConfigOptions): OAuthConfigIntern
   }
 }
 
+/**
+ * Normalize OAuth endpoints.
+ */
 function normalizeEndpoint<T extends keyof OAuthConfig<any>>(
   e?: OAuthConfig<any>[T],
   issuer?: string
