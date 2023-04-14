9c25cd04a749614ea25bcfdf5824bd40763f6572
senti <brianmv@uci.edu>
2023-04-14 15:44:32 -0700

    simplified oauth interfaces

--- packages/core/src/lib/providers/index.ts
+++ packages/core/src/lib/providers/index.ts
@@ -1,63 +1,31 @@
 import type * as oauth from 'oauth4webapi'
 import type { CookiesOptions, Profile, TokenSet, User } from '@auth/core/types'
-import * as checks from '$lib/integrations/check'
-import type { Cookie, InternalRequest, InternalResponse } from '$lib/integrations/response'
+import type { InternalRequest, InternalResponse } from '$lib/integrations/response'
 import type { JWTOptions } from '$lib/jwt'
 
 type Awaitable<T> = T | PromiseLike<T>
 
+/**
+ * All providers have these methods.
+ */
 export interface Provider<T extends AnyInternalConfig> {
   config: T
 
-  signIn(request: InternalRequest): Promise<InternalResponse>
+  /**
+   * Handle sign-in attempt with provider.
+   */
+  signIn(request: InternalRequest): Awaitable<InternalResponse>
 
-  callback(request: InternalRequest): Promise<InternalResponse>
+  /**
+   * Follow-up after sign-in. 
+   * Only OAuth providers have this method. Ignore for other providers.
+   */
+  callback(request: InternalRequest): Awaitable<InternalResponse>
 
-  signOut(request: InternalRequest): Promise<InternalResponse>
-}
-
-/**
- * Common procedure for getting authorization URL for OAuth and OIDC providers.
- */
-export async function handleOAuthUrl(
-  request: InternalRequest,
-  provider: InternalOAuthConfig | InternalOIDCConfig
-) {
-  const cookies: Cookie[] = []
-  const { url } = provider.authorization
-
-  if (provider.checks?.includes('state')) {
-    const [state, stateCookie] = await checks.state.create(provider)
-    url.searchParams.set('state', state)
-    cookies.push(stateCookie)
-  }
-
-  if (provider.checks?.includes('pkce')) {
-    if (provider.authorizationServer.code_challenge_methods_supported?.includes('S256')) {
-      provider.checks = ['nonce']
-    } else {
-      const [pkce, pkceCookie] = await checks.pkce.create(provider)
-      url.searchParams.set('code_challenge', pkce)
-      url.searchParams.set('code_challenge_method', 'S256')
-      cookies.push(pkceCookie)
-    }
-  }
-
-  if (provider.checks?.includes('nonce')) {
-    const [nonce, nonceCookie] = await checks.nonce.create(provider)
-    url.searchParams.set('nonce', nonce)
-    cookies.push(nonceCookie)
-  }
-
-  if (!url.searchParams.has('redirect_uri')) {
-    url.searchParams.set('redirect_uri', `${request.url.origin}/callback/${provider.id}`)
-  }
-
-  if (provider.type === 'oidc' && !url.searchParams.has('scope')) {
-    url.searchParams.set("scope", "openid profile email")
-  }
-
-  return { redirect: url.toString(), cookies }
+  /**
+   * Handle sign-out attempt with provider.
+   */
+  signOut(request: InternalRequest): Awaitable<InternalResponse>
 }
 
 /**
@@ -70,10 +38,7 @@ export async function handleOAuthUrl(
  */
 export type ProviderType = "oidc" | "oauth" | "email" | "credentials"
 
-/**
- * Any internally generated OAuth-based config.
- */
-export type AnyInternalOAuthConfig = InternalOAuthConfig | InternalOIDCConfig
+export type OAuthProviderType = "oidc" | "oauth"
 
 /**
  * Any internally generated config.
@@ -106,10 +71,13 @@ interface ProviderConfig {
 }
 
 /**
- * Internally generated config for OAuth providers from Auth.js .
+ * General interface for OAuth providers, e.g. OAuth, OIDC.
  */
-export interface InternalOAuthConfig extends ProviderConfig {
-  type: 'oauth'
+export interface AnyInternalOAuthConfig<T extends OAuthProviderType = OAuthProviderType> extends ProviderConfig {
+  /**
+   * OAuth type.
+   */
+  type: T
 
   /**
    * OAuth Authorization Server.
@@ -121,10 +89,19 @@ export interface InternalOAuthConfig extends ProviderConfig {
    */
   client: oauth.Client
 
+  /**
+   * Generate cookie options.
+   */
   cookies: CookiesOptions
 
+  /**
+   * Override default JWT options.
+   */
   jwt: JWTOptions
 
+  /**
+   * Which OAuth checks to perform.
+   */
   checks: Array<"pkce" | "state" | "none" | "nonce">
 
   /**
@@ -177,91 +154,24 @@ export interface InternalOAuthConfig extends ProviderConfig {
     ) => Awaitable<Profile>
   }
 }
+
+
+/**
+ * Internally generated config for OAuth providers from Auth.js .
+ */
+export interface InternalOAuthConfig extends AnyInternalOAuthConfig<"oauth"> {}
 
 /**
  * Internally generated config for OIDC providers from Auth.js .
  */
-export interface InternalOIDCConfig extends ProviderConfig {
-  type: 'oidc'
-
-  /**
-   * OAuth Authorization Server.
-   */
-  authorizationServer: oauth.AuthorizationServer
-
-  /**
-   * Client to make requests.
-   */
-  client: oauth.Client
-
-  checks: Array<"pkce" | "state" | "none" | "nonce">
-
-  cookies: CookiesOptions
-
-  jwt: JWTOptions
-
-  /**
-   * Receives the profile object returned by the OAuth provider, and returns a user object.
-   */
-  profile: (profile: Profile, tokens: TokenSet) => Awaitable<User>
-
-  /**
-   * Additional data about the authorization endpoint.
-   */
-  authorization: { 
-
-    /**
-     * Full URL to authorization endpoint with search params.
-     */
-    url: URL
-  }
-
-  /**
-   * Additional data about the token endpoint.
-   */
-  token: {
-
-    /**
-     * Full URL to token endpoint.
-     */
-    url: URL
-
-    /**
-     * Process the response from the token endpoint.
-     */
-    conform: (response: Response) => Awaitable<Response>
-  }
-
-  /**
-   * Additional data about the userinfo endpoint.
-   */
-  userinfo: {
-
-    /**
-     * Full URL to userinfo endpoint.
-     */
-    url: URL
-
-    /**
-     * Make a request to the userinfo endpoint.
-     */
-    request: (
-      context: { tokens: TokenSet, provider: InternalOAuthConfig | InternalOIDCConfig }
-    ) => Awaitable<Profile>
-  }
-}
+export interface InternalOIDCConfig extends AnyInternalOAuthConfig<"oidc"> {}
 
 /**
  * Internally generated config for email authentication.
  */
-export interface InternalEmailConfig extends ProviderConfig {
-  type: 'email'
-}
+export interface InternalEmailConfig extends ProviderConfig { type: 'email' }
 
 /**
  * Internally generated config for credentials authentication.
  */
-export interface InternalCredentialsConfig extends ProviderConfig {
-  type: 'credentials'
-}
-
+export interface InternalCredentialsConfig extends ProviderConfig { type: 'credentials' }
--- packages/core/src/lib/providers/oidc.ts
+++ packages/core/src/lib/providers/oidc.ts
@@ -1,7 +1,6 @@
 import * as oauth from 'oauth4webapi'
 import type { Cookie, InternalRequest, InternalResponse } from '$lib/integrations/response'
 import * as checks from '$lib/integrations/check'
-import { handleOAuthUrl } from '.'
 import type { Provider, InternalOIDCConfig } from '.';
 
 
@@ -9,7 +8,42 @@ export class OIDCProvider implements Provider<InternalOIDCConfig> {
   constructor(readonly config: InternalOIDCConfig) {}
 
   async signIn(request: InternalRequest): Promise<InternalResponse> {
-    return handleOAuthUrl(request, this.config)
+    const provider = this.config
+    const cookies: Cookie[] = []
+    const { url } = provider.authorization
+
+    if (provider.checks?.includes('state')) {
+      const [state, stateCookie] = await checks.state.create(provider)
+      url.searchParams.set('state', state)
+      cookies.push(stateCookie)
+    }
+
+    if (provider.checks?.includes('pkce')) {
+      if (provider.authorizationServer.code_challenge_methods_supported?.includes('S256')) {
+        provider.checks = ['nonce']
+      } else {
+        const [pkce, pkceCookie] = await checks.pkce.create(provider)
+        url.searchParams.set('code_challenge', pkce)
+        url.searchParams.set('code_challenge_method', 'S256')
+        cookies.push(pkceCookie)
+      }
+    }
+
+    if (provider.checks?.includes('nonce')) {
+      const [nonce, nonceCookie] = await checks.nonce.create(provider)
+      url.searchParams.set('nonce', nonce)
+      cookies.push(nonceCookie)
+    }
+
+    if (!url.searchParams.has('redirect_uri')) {
+      url.searchParams.set('redirect_uri', `${request.url.origin}/callback/${provider.id}`)
+    }
+
+    if (!url.searchParams.has('scope')) {
+      url.searchParams.set("scope", "openid profile email")
+    }
+
+    return { redirect: url.toString(), cookies }
   }
 
   async callback(request: InternalRequest): Promise<InternalResponse> {
