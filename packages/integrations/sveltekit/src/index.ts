16ad976003f41b68d08880f19f6689e8997a3a72
senti <brianmv@uci.edu>
2023-04-27 21:00:13 -0700

    nested user under locals

--- packages/integrations/sveltekit/src/index.ts
+++ packages/integrations/sveltekit/src/index.ts
@@ -2,10 +2,20 @@ import type { Auth } from 'aponia'
 import { json, redirect } from '@sveltejs/kit'
 import type { Handle } from '@sveltejs/kit'
 
+export interface Options {
+  /**
+   * Which key to store the user under in locals, if found during hook.
+   */
+  localsUserKey?: string
+}
+
 const validRedirect = (status?: number): status is Parameters<typeof redirect>[0] =>
   status != null && status >= 300 && status < 400
 
-export function createAuthHandle<TUser, TSession, TRefresh>(auth: Auth<TUser, TSession, TRefresh>) {
+export function createAuthHandle<TUser, TSession, TRefresh>(
+  auth: Auth<TUser, TSession, TRefresh>,
+  options: Options = {}
+) {
   const handle: Handle = async ({ event, resolve }) => {
     const internalResponse = await auth.handle(event.request)
 
@@ -22,6 +32,8 @@ export function createAuthHandle<TUser, TSession, TRefresh>(auth: Auth<TUser, TS
     if (internalResponse.body != null) {
       return json(internalResponse.body)
     }
+
+    (event.locals as any)[options.localsUserKey ?? 'user'] = internalResponse.user
 
     return await resolve(event)
   }
