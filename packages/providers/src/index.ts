a1f4a3e48bfc5a9b96e2e175a4eda488d08f54be
senti <brianmv@uci.edu>
2023-05-04 12:42:10 -0700

    correct version bump

---
+++ packages/providers/src/index.ts
@@ -0,0 +1,7 @@
+export * from './auth0'
+export * from './discord'
+export * from './facebook'
+export * from './github'
+export * from './google'
+export * from './linkedin'
+export * from './twitch'
